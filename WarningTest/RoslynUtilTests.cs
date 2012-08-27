bf8393900c1130d598fecdc3e0c1bb3dac87a438
= <=>
2012-08-26 20:21:15 -0400

    add document analyzer

---
+++ WarningTest/RoslynUtilTests.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using NLog;
+using Roslyn.Services;
+using warnings.analyzer;
+using warnings.util;
+
+namespace WarningTest
+{
+    [TestClass]
+    public class RoslynUtilTests
+    {
+
+        [TestMethod]
+        public void TestMethod1()
+        {
+            ISolution solution = RoslynUtil.GetSolution(TestUtil.getSolutionPath());
+            Assert.IsNotNull(solution);
+        }
+
+        [TestMethod]
+        public void TestMethod2()
+        {
+            var solution = RoslynUtil.GetSolution(TestUtil.getSolutionPath());
+            var project = RoslynUtil.GetProject(solution, "WarningTest");
+            Assert.IsNotNull(project);
+        }
+
+        [TestMethod]
+        public void TestMethod3()
+        {
+            var solution = RoslynUtil.GetSolution(TestUtil.getSolutionPath());
+            var project = RoslynUtil.GetProject(solution, "WarningTest");
+            var document = RoslynUtil.GetDocument(project, "ASTUtilTests.cs");
+            Assert.IsNotNull(document);
+            document = RoslynUtil.GetDocument(project, "EMDetectorAfter.cs");
+            Assert.IsNotNull(document);
+        }
+
+        [TestMethod]
+        public void TestMethod4()
+        {
+            String updatedString = "Updated String";
+            var solution = RoslynUtil.GetSolution(TestUtil.getSolutionPath());
+            var project = RoslynUtil.GetProject(solution, "WarningTest");
+            var document = RoslynUtil.GetDocument(project, "TryToUpdate.cs");
+            Assert.IsNotNull(document);
+            solution = RoslynUtil.UpdateDocumentToString(document, updatedString);
+            project = RoslynUtil.GetProject(solution, "WarningTest");
+            document = RoslynUtil.GetDocument(project, "TryToUpdate.cs");
+            Assert.IsNotNull(document);
+            Assert.IsTrue(document.GetText().GetText().Equals(updatedString));
+        }
+
+        [TestMethod]
+        public void TestMethod5()
+        {
+            Logger logger = NLoggerUtil.getNLogger(typeof (RoslynUtilTests));
+            var solution = RoslynUtil.GetSolution(TestUtil.getSolutionPath());
+            var project = RoslynUtil.GetProject(solution, "WarningTest");
+            var document = RoslynUtil.GetDocument(project, "RoslynUtilTests.cs");
+            var analyzer = new DocumentAnalyzer(document);
+            Assert.IsNotNull(document);
+            try
+            {
+                logger.Info(analyzer.DumpSyntaxTree());
+            }catch( Exception e)
+            {
+                logger.Fatal(e);
+            }
+        }
+
+        [TestMethod]
+        public void TestMethod6()
+        {
+            Logger logger = NLoggerUtil.getNLogger(typeof (RoslynUtilTests));
+            var solution = RoslynUtil.GetSolution(TestUtil.getSolutionPath());
+            var project = RoslynUtil.GetProject(solution, "WarningTest");
+            Assert.IsNotNull(project);
+            foreach (IDocument document in project.Documents)
+            {
+                string name = document.Name;
+                logger.Info(name);
+            }
+        }
+    }
+}
---
+++ warnings/analyzer/DocumentAnalyzer.cs
@@ -0,0 +1,130 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using NLog;
+using Roslyn.Compilers.CSharp;
+using Roslyn.Compilers.Common;
+using Roslyn.Services;
+using warnings.util;
+
+namespace warnings.analyzer
+{
+    /* An anlayzer of the basic structure for the given document. */
+    internal interface IDocumentAnalyzer
+    {
+        IEnumerable GetNamespaceDecalarations();
+        IEnumerable GetClassDeclarations(NamespaceDeclarationSyntax mamespaceDeclaration);
+        IEnumerable GetFieldDeclarations(ClassDeclarationSyntax classDeclaration);
+        IEnumerable GetMethodDeclarations(ClassDeclarationSyntax classDeclaration);
+        IEnumerable GetVariableDeclarations(MethodDeclarationSyntax methodDeclaration);
+        String DumpSyntaxTree();
+    }
+
+    public class DocumentAnalyzer : IDocumentAnalyzer
+    {
+        private IDocument document;
+
+        private Logger logger;
+
+        private SyntaxNode root;
+
+        public DocumentAnalyzer(IDocument document)
+        {
+            this.document = document;
+            this.root = (SyntaxNode)document.GetSyntaxRoot();
+            this.logger = NLoggerUtil.getNLogger(typeof (DocumentAnalyzer));
+        }
+
+        public IEnumerable GetNamespaceDecalarations()
+        {
+            return GetDecendantOfKind(root, SyntaxKind.NamespaceDeclaration);
+         }
+
+        public IEnumerable GetClassDeclarations(NamespaceDeclarationSyntax mamespaceDeclaration)
+        {
+            return GetDecendantOfKind(mamespaceDeclaration, SyntaxKind.ClassDeclaration);
+        }
+
+        public IEnumerable GetFieldDeclarations(ClassDeclarationSyntax classDeclaration)
+        {
+            return GetDecendantOfKind(classDeclaration, SyntaxKind.FieldDeclaration);
+        }
+
+        public IEnumerable GetMethodDeclarations(ClassDeclarationSyntax classDeclaration)
+        {
+            return GetDecendantOfKind(classDeclaration, SyntaxKind.MethodDeclaration);
+        }
+
+        public IEnumerable GetVariableDeclarations(MethodDeclarationSyntax methodDeclaration)
+        {
+            return GetDecendantOfKind(methodDeclaration, SyntaxKind.VariableDeclaration);
+        }
+
+        public string DumpSyntaxTree()
+        {
+            StringBuilder sb = new StringBuilder();
+            sb.AppendLine();
+
+            // Iterate each namespace in the file.
+            foreach (NamespaceDeclarationSyntax namespaceDec in GetNamespaceDecalarations())
+            {
+                sb.AppendLine(namespaceDec.Name.PlainName);
+                
+                // Iterate the classes in each namepace.
+                foreach (ClassDeclarationSyntax classDec in GetClassDeclarations(namespaceDec))
+                {
+                    sb.Append("\t");
+                    sb.AppendLine(classDec.Identifier.ValueText);
+                    
+                    // Iterate the field declarations in each class, print only the first one.
+                    foreach (FieldDeclarationSyntax field in GetFieldDeclarations(classDec))
+                    {
+                        sb.Append("\t\t");
+                        sb.AppendLine(field.Declaration.Variables[0].Identifier.ValueText);
+                    }
+
+                    // Iterate the methods declarations in each class.
+                    foreach (MethodDeclarationSyntax method in GetMethodDeclarations(classDec))
+                    {
+                        sb.Append("\t\t");
+                        sb.AppendLine(method.Identifier.ValueText);
+
+                        // Iterate the local variable declarations in each method, print only the first one
+                        // in a declaration.
+                        foreach (VariableDeclarationSyntax variable in GetVariableDeclarations(method))
+                        {
+                            sb.Append("\t\t\t");
+                            sb.AppendLine(variable.Variables[0].Identifier.ValueText);
+                        }
+                    }
+                }
+            }
+            return sb.ToString();
+        }
+
+        private IEnumerable GetDecendantOfKind(SyntaxNode parent, SyntaxKind kind)
+        {
+            return parent.DescendantNodes().Where( n => n.Kind == kind);
+         }
+
+    }
+
+
+    internal class AnalyzerWalker : SyntaxWalker
+    {
+        private Logger logger;
+
+        public AnalyzerWalker()
+        {
+            this.logger = NLoggerUtil.getNLogger(typeof (AnalyzerWalker));
+        }
+
+        public override void Visit(SyntaxNode node)
+        {
+            base.Visit(node);
+            logger.Info(node.ToString());
+        }
+    }
+}
